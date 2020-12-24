44d011093a5b6ce53702494632c634633d79897a
Antonello Paoletti <antonello.paoletti@infn.it>
2020-12-24 15:37:33 +0100

    db

--- _helpers/db.js
+++ _helpers/db.js
@@ -9,12 +9,12 @@ initialize();
 async function initialize() {
     // create db if it doesn't already exist
     const { host, port, user, password, database } = config.database;
-    //const connection = await mysql.createConnection({ host, port, user, password });
-    //await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
+    const connection = await mysql.createConnection({ host, port, user, password });
+    await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
 
     // connect to db
-    //const sequelize = new Sequelize(database, user, password, { host: '192.168.1.9', dialect: 'mysql' });
-    const sequelize = new Sequelize({dialect: 'sqlite', storage: 'data/dev-db.sqlite3', logging: false})
+    const sequelize = new Sequelize(database, user, password, { host: '192.168.1.9', dialect: 'mysql' });
+    //const sequelize = new Sequelize({dialect: 'sqlite', storage: 'data/dev-db.sqlite3', logging: false})
     
     // init models and add them to the exported db object
     db.User = require('../users/user.model')(sequelize);
